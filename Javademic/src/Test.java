b2a5adc46202a61bdef3c76f59d04846372018d0
Brian Jerardi <>
2011-11-23 13:40:45 -0500

    Added Test class to call other test classes

---
+++ Javademic/src/Test.java
@@ -0,0 +1,21 @@
+
+import Disease.DiseaseUnitTest;
+
+/**
+ * Calls test classes
+ * @author Brian Jerardi
+ * @instructor Kendall Martin
+ * @date 11/23/2011
+ * @course CIS 112
+ */
+public class Test
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {               
+        DiseaseUnitTest.main(args);
+    }
+}
