99b5e85b08fe321ab7ceaf5bdd9b04acd5eee0fc
Bruce Stringer <bruce.stringer.it@gmail.com>
2011-12-05 22:34:43 -0500

    A driver to be used for later testing. Simply initializes the board.

---
+++ src/LocationTestDriver.java
@@ -0,0 +1,11 @@
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Gifflen
+ */
+public class LocationTestDriver {
+
+    public static void main(String[] args){
+         Board gameBoard = new Board();
+    }
+}
