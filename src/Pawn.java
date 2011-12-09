de4cb07db9208a68f3e60ae996b56e9c36ec922d
Bruce Stringer <bruce.stringer.it@gmail.com>
2011-12-08 22:32:48 -0500

    Basic pawn class to contain information about where it is located

---
+++ src/Pawn.java
@@ -0,0 +1,16 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: Gifflen
+ */
+public class Pawn {
+    Location currentLocation;
+
+    public Location getCurrentLocation() {
+        return currentLocation;
+    }
+
+    public void setCurrentLocation(Location currentLocation) {
+        this.currentLocation = currentLocation;
+    }
+
+}
