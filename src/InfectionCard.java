b10e4b48774b47e42702f6102ded21468de8eba5
Bruce Stringer <bruce.stringer.it@gmail.com>
2011-12-08 22:31:39 -0500

    Base  epidemic card to contain information location it is associated with

---
+++ src/InfectionCard.java
@@ -0,0 +1,15 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: Gifflen
+ */
+public class InfectionCard {
+    Location location;
+
+    public InfectionCard(Location location) {
+        this.location = location;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+}
