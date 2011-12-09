58b09b2b788d6cfaec50e593e453f12a8c69a646
Bruce Stringer <bruce.stringer.it@gmail.com>
2011-12-08 22:33:50 -0500

    Many updates to the process of how the board is initialized. Far from done but has some work for that we can work off of.

--- src/Board.java
+++ src/Board.java
@@ -1,7 +1,8 @@
 import java.io.*;
+import java.util.Deque;
 import java.util.HashMap;
+import java.util.Scanner;
 import java.util.Map;
-
 
 /**
  * Created by IntelliJ IDEA.
@@ -9,35 +10,95 @@ import java.util.Map;
  */
 public class Board {
     private HashMap<String,Location> locations =  new HashMap<String, Location>();
-    private File locationCSV = new File("C:\\Users\\Gifflen\\IdeaProjects\\Javademic\\src\\Locations.csv");
+    private File locationCSV = new File("src\\Locations.csv");
+    private Location defaultLocation;
+    private int numPlayers;
+    private int difficulty;
+    final String DEFAULT_LOCATION_NAME = "Atlanta";
+    private Pawn[] players;
+    private Deque<PlayerCard> playerDeck;
+    private Deque<InfectionCard> infectionDeck;
+
+
+
+
+    public int getDifficulty() {
+        return difficulty;
+    }
+
+    public Location getDefaultLocation() {
+
+        return defaultLocation;
+    }
+
+    private void setDefaultLocation(Location defaultLocation) {
+        this.defaultLocation = defaultLocation;
+    }
+
+    public int getNumPlayers() {
+        return numPlayers;
+    }
 
     public Board() {
 
-        this.setUpLocations();
-        for (Map.Entry<String, Location> stringLocationEntry : locations.entrySet()) {
-            Map.Entry pairs;
-            pairs = stringLocationEntry;
-            Location test = (Location) pairs.getValue();
-            if (test.listLocations().length() < 1) {
-                System.out.println(test.toString());
-            }
-            //System.out.println(pairs.getKey() +  " = " + pairs .getValue());
-        }
+        this.initLocations();
+        this.initPlayers();
+        this.initDifficulty();
+        this.initDeck();
+
     }
 
     public Location addLocationToBoard(String locationName){
         locationName = Location.cleanString(locationName);
-        if (!locations.containsKey(locationName)){
-           //System.out.println("Adding " + locationName);
-           locations.put(locationName,new Location(locationName));
+        if (!locations.containsKey(locationName)) {
+            System.out.println("Adding " + locationName);
+            locations.put(locationName, new Location(locationName));
+            return locations.get(locationName);
+        } else {
+            return locations.get(locationName);
         }
-        return locations.get(locationName);
     }
 
-    public void setUpLocations(){
+    private void initLocations(){
+        System.out.println("Initializing Locations...");
         locations = new HashMap<String,Location>();
         readCSV();
+        this.setDefaultLocation(this.getLocationByName(DEFAULT_LOCATION_NAME));
     }
+
+    public void initPlayers(){
+        System.out.println("Initializing Players...");
+        numPlayers = getIntInput("How many players will play in this game? 2-4: ", 2, 4);
+        System.out.println("You've Chosen to play with " + numPlayers + " Players.");
+        players = new Pawn[numPlayers];
+        //Choose Roles
+        //Determine Turn Order
+        //Sort pawns
+    }
+
+    public void initDifficulty(){
+        System.out.println("Initializing Difficulty...");
+        difficulty = getIntInput("Please Select Difficulty Level\n" +
+                            "1. Introductory\n" +
+                            "2. Normal \n" +
+                            "3. Heroic",1,3);
+       System.out.println("You've Chosen to play at the " + difficulty + " difficulty level");
+    }
+
+    public void initDeck(){
+        System.out.println("Initializing Decks...");
+
+        for (Location location : locations.values()) {
+            PlayerCard playerCard = new PlayerCard(location);
+            InfectionCard  infectionCard = new InfectionCard(location);
+            playerDeck.push(playerCard);
+            infectionDeck.push(infectionCard);
+        }
+
+        //Shuffle Deck
+        System.out.println("NOT IMPLEMENTED YET");
+    }
+
 
     public Location getLocationByName(String locationName){
         return locations.get(Location.cleanString(locationName));
@@ -61,10 +122,10 @@ public class Board {
                     if (field[fieldNumber].length()>0){
                         Location connection =  addLocationToBoard(field[fieldNumber]);
                         currentLocation.addConnection(connection);
+                        System.out.println("\t Adding Connection.getLocationName(): " + connection);
                     }
                     fieldNumber++;
                 }
-                System.out.println(currentLocation);
             }
             reader.close();
         } catch (FileNotFoundException e) {
@@ -73,6 +134,26 @@ public class Board {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+    }
+
+
+    private int getIntInput(String message,int lowBound,int highBound){
+        Scanner userInputScanner = new Scanner(System.in);
+        String userInput = "";
+        int inputInt=0;
+        do{
+            System.out.println(message);
+
+            userInput = userInputScanner.next();
+
+            try{
+                inputInt = Integer.parseInt(userInput);
+            }catch (NumberFormatException e ){
+                System.out.println("Please input an integer.");
+            }
+        } while (((inputInt<lowBound)||(inputInt>highBound)));
+        return inputInt;
     }
 
 
