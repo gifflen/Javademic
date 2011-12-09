0fc647f66293a50ae1a810d33c7dd03bfba095a8
Bruce Stringer <bruce.stringer.it@gmail.com>
2011-12-08 22:31:04 -0500

    Base player care to contain information location it is associated with

---
+++ src/PlayerCard.java
@@ -0,0 +1,17 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: Gifflen
+ */
+public class PlayerCard {
+    Location location;
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public PlayerCard(Location location) {
+        this.location = location;
+
+
+    }
+}
