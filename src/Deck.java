da661090d18c4b7ff4b2453246147b0f9eea19a8
Bruce Stringer <buggerxenocide@gmail.com>
2011-12-12 22:59:29 -0500

    Cleaned up some unneeded imports.

--- src/Deck.java
+++ src/Deck.java
@@ -1,14 +1,9 @@
 import java.util.Collections;
-import java.util.Deque;
-import java.util.List;
 import java.util.Stack;
 
 /**
  * Created by IntelliJ IDEA.
  * User: Gifflen
- * Date: 12/12/11
- * Time: 8:32 PM
- * To change this template use File | Settings | File Templates.
  */
 public class Deck<E> extends Stack {
 
@@ -21,17 +16,9 @@ public class Deck<E> extends Stack {
         return lastElement;
     }
     public void shuffle(){
-        //List cardList = this.subList(0, this.elementCount - 1);
-        //System.out.println(cardList);
         System.out.println(this);
         Collections.shuffle(this);
         System.out.println(this);
-        //this.clear();
-
-        //for(Object card : cardList){
-        //   System.out.println(card);
-         //  this.addElement(card);
-       //}
     }
 
 
