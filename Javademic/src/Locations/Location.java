78953de7f65f2888d07b2c29cf2db4786656a198
Brian Jerardi <bjerardi@gmail.com>
2011-12-13 19:25:38 -0500

    Added Location Stub for ResearchStation Classes

---
+++ Javademic/src/Locations/Location.java
@@ -0,0 +1,21 @@
+package Locations;
+
+/**
+ * A stub for the Location class for testing
+ * @author Brian Jerardi
+ */
+public class Location
+{
+    private String name;
+    
+    public Location(String name)
+    {
+        this.name = name;
+    }
+    
+    public String getName()
+    {
+        return name;
+    }
+    
+}
