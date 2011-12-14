7eb85ad4edb351292fad285fc70cfceacf080393
Brian Jerardi <bjerardi@gmail.com>
2011-12-13 20:04:39 -0500

    Updated Test.java to run unit tests.

--- Javademic/src/Test.java
+++ Javademic/src/Test.java
@@ -1,6 +1,7 @@
 
-import UnitTests.DiseaseUnitTest;
-import UnitTests.DiseasesUnitTest;
+import UnitTests.ResearchStationCollectionTest;
+import UnitTests.ResearchStationTest;
+
 
 /**
  * Calls test classes
@@ -17,7 +18,8 @@ public class Test
      */
     public static void main(String[] args)
     {              
-        DiseaseUnitTest.main(args);
-        DiseasesUnitTest.main(args);
+        ResearchStationTest.main(args);
+        ResearchStationCollectionTest.main(args);
+        
     }
 }
