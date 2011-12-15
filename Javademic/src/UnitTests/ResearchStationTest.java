9fb3425de02910dd7690abf942f773cf8c3ac3bc
Brian Jerardi <bjerardi@gmail.com>
2011-12-14 22:19:38 -0500

    Added Research Station Class Tests

---
+++ Javademic/src/UnitTests/ResearchStationTest.java
@@ -0,0 +1,36 @@
+package UnitTests;
+
+import Locations.Location;
+import ResearchStation.ResearchStation;
+
+/**
+ * Unit Test for ResearchStation Class
+ * @author Brian Jerardi
+ */
+public class ResearchStationTest
+{
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {              
+        Location loc1 = new Location("Test Loc 1");
+        Location loc2 = new Location("Test Loc 2");
+        ResearchStation station = new ResearchStation(loc1);
+        
+        System.out.println("### ResearchStation Test ###\n");
+        
+        System.out.println("Get the ResearchStation's Location");
+        System.out.println("Location: " + station.getLocation().getLocationName());
+        System.out.println(""); 
+        
+        System.out.println("Set the ResearchStation's Location");
+        station.setLocation(loc2);
+        System.out.println("Location: " + station.getLocation().getLocationName());
+        System.out.println(""); 
+        
+        
+        System.out.println("### End ResearchStation Test ###");
+    }
+    
+}
